750b8563211e3ef698762b8defc60e7b586cf2f0
lou <lfs4@njit.edu>
2014-05-07 11:15:20 -0400

    improved performance by removing container element

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -2,21 +2,30 @@
         var localdata = JSON.parse($("#localdata").val());
         var mapOptions = {
           center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 2
+          zoom: 1
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
 
         //var myLatLang = new google.maps.LatLng(-25.363882,131.044922);
-        for(var i = 0; i < localdata.length; i++){
-          var meteor = localdata[i];
-        	var myLatLang = new google.maps.LatLng(meteor.latitude, meteor.longitude);
-	        var marker = new google.maps.Marker({
-	        	position: myLatLang,
-	        	map: map,
-	        	//animation: google.maps.Animation.DROP,
-	        	title: "Hello World"
-	        });
-    	}
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+      for(var i = 0; i < 20; i++){
+            var meteor = localdata[i];
+          	var myLatLang = new google.maps.LatLng(meteor.latitude, meteor.longitude);
+  	        var marker = new google.maps.Marker({
+              classification: meteor.classification,
+              year: meteor.year,
+              mass: meteor.mass,
+              fell: meteor.fall,
+  	        	position: myLatLang,
+  	        	map: map,
+  	        	animation: google.maps.Animation.DROP,
+  	        	title: "Hello World"
+  	        });
+    	 }
+       $('#localdata').remove();
+       google.maps.event.addListener(marker, 'click', function(){
+          console.log(marker.classification);
+       });
+    }
+
+google.maps.event.addDomListener(window, 'load', initialize);
