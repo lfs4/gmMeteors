b95a21db87c0d9f86e26d4e6be687fdea5ec2423
lou <lfs4@njit.edu>
2014-05-09 16:56:03 -0400

    added map controller

---
+++ controllers/map.js
@@ -0,0 +1,9 @@
+// exports.initialize = function(req,res){
+// 	var mapOptions = {
+// 		center: new googlemaps.LatLang(-34.397, 150.644),
+// 		zoom: 8
+// 	});
+// 		res.render('map',mapOptions);
+// 	};
+// 	google.maps.event.addDomListener(window, 'load', initialize);
+// }
